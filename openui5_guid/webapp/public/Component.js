1a4e20a8ec5cfce16771a9c5b5e285133a9ac4d5
leolewin <leolewin@live.cn>
2016-10-17 11:21:22 +0800

    app setting configuration added.

--- openui5_guid/webapp/public/Component.js
+++ openui5_guid/webapp/public/Component.js
@@ -2,11 +2,12 @@ sap.ui.define([
 	"sap/ui/core/UIComponent",
 	"sap/ui/model/json/JSONModel",
 	"sap/ui/model/resource/ResourceModel"],
-	function(UIComponent, JSONModel, ResourceModel){
+	function(UIComponent, JSONModel){
 		"use strict";
 		return UIComponent.extend("sap.ui.demo.wt.Component", {
 			metadata : {
-				rootView : "sap.ui.demo.wt.view.App"
+				// rootView : "sap.ui.demo.wt.view.App"
+				manifest : "json"
 			},
 			init : function(){
 				UIComponent.prototype.init.apply(this, arguments);
@@ -17,10 +18,10 @@ sap.ui.define([
 				var oModel = new JSONModel(oData);
 				this.setModel(oModel);
 
-				var i18nModel = new ResourceModel({
-					bundleName : "sap.ui.demo.wt.i18n.i18n"
-				});
-				this.setModel(i18nModel, "i18n");
+				// var i18nModel = new ResourceModel({
+				// 	bundleName : "sap.ui.demo.wt.i18n.i18n"
+				// });
+				// this.setModel(i18nModel, "i18n");
 			}
 		});
 	}
