5074b01c94e9b5188dd4a2e1729d955f4ecb0735
leolewin <leolewin@live.cn>
2016-10-19 17:34:52 +0800

    添加了虚拟server 用于单元测试

--- openui5_guid/webapp/routes/test.js
+++ openui5_guid/webapp/routes/test.js
@@ -4,7 +4,8 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // res.render('index', { title: 'Express' });
-	res.sendfile('index.html');
+	res.sendfile('./public/test/mockServer.html');
+
 });
 
 module.exports = router;
